293265335c65dd5ea6bf2c9062fc0dfcc2a25dd3
Bezawork <bezaworkalx@gmail.com>
2023-05-03 11:12:10 +0000

    Create table with int value, varchar and ENUM

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a table users
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email Varchar(255) NOT NULL UNIQUE,
+	name Varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
