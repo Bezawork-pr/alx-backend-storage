606d8400f0b363a2b81c4ef889bd18c795bf22d4
Bezawork <bezaworkalx@gmail.com>
2023-05-07 07:11:29 +0000

    Add IF NOT EXISTS so CREATE TABLE doesnt fail if table exists

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that creates a table users
-CREATE TABLE users (
+CREATE IF NOT EXISTS TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email Varchar(255) NOT NULL UNIQUE,
 	name Varchar(255),
