aa5f9e64fd525c25ce5165773180d5055669f252
Bezawork <bezaworkalx@gmail.com>
2023-05-08 10:36:19 +0000

    Create view for table students

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+-- Write a SQL script that creates a view need_meeting
+CREATE VIEW need_meeting AS
+SELECT *
+FROM students
+WHERE score < 80 AND last_meeting IS NULL OR DATEDIFF(NOW(), last_meeting) > 31;
