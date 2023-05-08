5e75c24d4a28a9356b52a48951490c93d43a2d5b
Bezawork <bezaworkalx@gmail.com>
2023-05-08 10:39:57 +0000

    Select only name from table for view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that creates a view need_meeting
 CREATE VIEW need_meeting AS
-SELECT *
+SELECT name
 FROM students
 WHERE score < 80 AND last_meeting IS NULL OR DATEDIFF(NOW(), last_meeting) > 31;
