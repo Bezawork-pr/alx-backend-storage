bf751e4d3bcd6a563f558d4ffe743ffd33cb4297
Bezawork <bezaworkalx@gmail.com>
2023-05-12 02:54:57 -0700

    Take dictionary outside loop for summing average

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -10,17 +10,15 @@ def top_students(mongo_collection):
     my_list = list(mongo_collection.find())
     my_sum = 0
     average_score_list = []
-    count = 0
     for i in my_list:
-        count += 1
         topics_dict = i["topics"]
         for m in range(len(topics_dict)):
             my_sum += topics_dict[m]["score"]
-            average_score_dict = {}
-            average_score_dict["_id"] = i["_id"]
-            average_score_dict["name"] = i["name"]
-            average_score_dict["averageScore"] = my_sum / len(topics_dict)
-            average_score_list.append(average_score_dict)
+        average_score_dict = {}
+        average_score_dict["_id"] = i["_id"]
+        average_score_dict["name"] = i["name"]
+        average_score_dict["averageScore"] = my_sum / len(topics_dict)
+        average_score_list.append(average_score_dict)
         my_sum = 0
     newlist = sorted(average_score_list, key=lambda d: d['averageScore']) 
     return reversed(newlist)
